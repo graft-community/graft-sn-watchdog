7f6904324da37891386769572b29efe6e42c5095
theck5263 <40315039+theck5263@users.noreply.github.com>
2019-03-21 20:42:41 +1000

    Update gn.sh

--- gn.sh
+++ gn.sh
@@ -3,7 +3,7 @@
 
 while true; do
         if [ ! `pgrep graftnoded` ]; then
-                ~/supernode/BUILD/bin/graftnoded --testnet --detach &
+                ~/Graftnetwork/graftnoded --detach &
                 for ((i=0; i<600; i+=5)); do
                         sleep 5
                         if [ ! `pgrep graftnoded` ]; then
